4bc69a05fb3bf1bf4bd8e2be3fe7ce5fc1f49c47
jdev <20902932+osekojoe@users.noreply.github.com>
2023-02-28 08:32:08 -0800

    betty fix

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -3,7 +3,7 @@
  * binary_tree_delete - deletes an entire binary tree
  * If tree is NULL, do nothing
  * @tree: pointer to the root node of the tree to delete
- * 
+ *
  * Return: None
  */
 void binary_tree_delete(binary_tree_t *tree)
@@ -15,9 +15,9 @@ void binary_tree_delete(binary_tree_t *tree)
 			binary_tree_delete(tree->left);
 		}
 		if (tree->right != NULL)
-                {
-                        binary_tree_delete(tree->right);
-                }
+		{
+			binary_tree_delete(tree->right);
+		}
 		free(tree);
 	}
 }
