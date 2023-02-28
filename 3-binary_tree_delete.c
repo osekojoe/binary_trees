dfb527d09b3672f3f3d0e8966a1d188def03847f
jdev <20902932+osekojoe@users.noreply.github.com>
2023-02-28 04:14:48 -0800

    delete

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_delete - deletes an entire binary tree
+ * If tree is NULL, do nothing
+ * @tree: pointer to the root node of the tree to delete
+ * 
+ * Return: None
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree != NULL)
+	{
+		if (tree->left != NULL)
+		{
+			binary_tree_delete(tree->left);
+		}
+		if (tree->right != NULL)
+                {
+                        binary_tree_delete(tree->right);
+                }
+		free(tree);
+	}
+}
