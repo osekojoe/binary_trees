13e32877b110a24a217dd20e0695b56f732094c0
jdev <20902932+osekojoe@users.noreply.github.com>
2023-03-01 01:41:37 -0800

    betty fix

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -16,8 +16,8 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 		}
 		func(tree->n);
 		if (tree->right)
-                {
-                        binary_tree_inorder(tree->right, func);
-                }
+		{
+			binary_tree_inorder(tree->right, func);
+		}
 	}
 }
